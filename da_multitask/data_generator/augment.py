fix rotation

da_multitask/data_generator/augment.py
@@ -101,13 +101,13 @@ class Rotate(Augmenter):
 
         Args:
             p: probability to apply this augmenter each time it is called
-            angle_range: the angle is randomised within this range;
+            angle_range: (degree) the angle is randomised within this range;
                 if this is a list, randomly pick an angle in this range;
                 if it's a float, the range is [-float, float]
         """
         super().__init__(p=p)
 
-        self.angle_range = format_range(angle_range, start_0=False)
+        self.angle_range = format_range(angle_range, start_0=False) / 180 * np.pi
 
     def _apply_logic(self, org_data: np.ndarray) -> np.ndarray:
         """
@@ -185,10 +185,6 @@ class TimeWarp(Augmenter):
 
         tt_cum *= t_scale
         return tt_cum
-
-    def _copy_data(self, org_data: np.ndarray) -> np.ndarray:
-        # don't need to copy because `_apply_logic` doesn't modify org_data
-        return org_data
 
     def _apply_logic(self, org_data: np.ndarray) -> np.ndarray:
         # create new timestamp for all channels

da_multitask/networks/conv.py
@@ -4,15 +4,28 @@ import torch.nn as nn
 
 class Conv1dBlock(nn.Module):
 
-    def __init__(self, input_len: Union[int, None], in_filters: int, out_filters: int,
+    def __init__(self, in_filters: int, out_filters: int,
                  kernel_size: int, stride: int = 1,
                  padding: Union[int, list, tuple] = 0, dilation: int = 1,
                  drop_rate: float = 0., use_spatial_dropout: bool = False,
-                 activation=nn.ReLU(), norm_layer: Union[str, None] = 'batch'):
-        super().__init__()
+                 activation=nn.ReLU(), norm_layer: Union[str, None] = 'batch', input_len: Union[int, None] = None):
         """
         Conv with padding, activation, batch norm
+
+        Args:
+            input_len: length of the input window, this is only necessary if `norm_layer` is 'layer'
+            in_filters:
+            out_filters:
+            kernel_size:
+            stride:
+            padding:
+            dilation:
+            drop_rate:
+            use_spatial_dropout:
+            activation:
+            norm_layer:
         """
+        super().__init__()
         conv = []
 
         if type(padding) is int:

script/exp4/train_2.py
@@ -20,6 +20,7 @@ from da_multitask.networks.classifier import MultiFCClassifiers
 def load_data(folder: str):
     """
     Load all data, 2 classes only (fall and non-fall)
+
     Args:
         folder:
 
@@ -30,33 +31,39 @@ def load_data(folder: str):
     train_dict_2 = defaultdict(list)  # D1 fall + D2, 1 D1 fall class + 21 D2 classes
     valid_dict = {0: [], 1: []}
 
-    files = glob(f'{folder}/D*/*.npy')
-    print(f'{len(files)} files found')
-
+    # GET D2
+    files = sorted(glob(f'{folder}/D2/*/*.npy'))
+    print(f'{len(files)} files found for D2')
     for file in files:
-        print(f'Reading file: {file}')
+        # print(f'Reading file: {file}')
         arr = np.load(file)[:, :, 1:]
-        valid_idx = np.arange(0, len(arr), 2)
-        train_idx = np.setdiff1d(np.arange(len(arr)), valid_idx)
 
-        # if this is D2 dataset, add to
-        if file.split(os.sep)[-2] == 'D2':
-            d2_class = re.search(r'_task([0-9][0-9]).npy$', file).group(1)
-            train_dict_2[f'D2_{d2_class}'].append(arr)
-        # get fall data
-        elif file.endswith('_fall.npy'):
-            train_dict_1[1].append(arr[train_idx])
-            train_dict_2['D1_1'].append(arr[train_idx])
+        d2_class = file.split('/')[-2]
+        train_dict_2[f'D2_{d2_class}'].append(arr)
 
-            valid_dict[1].append(arr[valid_idx])
-        # get all non-fall data
+    # GET D1, both train and valid
+    files = sorted(glob(f'{folder}/D1/*/*.npy'))
+    print(f'{len(files)} files found for D1')
+    for i, file in enumerate(files):
+        # print(f'Reading file: {file}')
+        arr = np.load(file)[:, :, 1:]
+
+        # use this file (sequence) for train or valid
+        is_train = (i % 2 == 0)
+        # get label: fall: 1; non-fall: 0
+        file_label = int(os.path.split(file)[0].endswith('_fall'))
+        
+        if is_train:
+            train_dict_1[file_label].append(arr)
+            # only add to train_dict_2 if this label is fall
+            if file_label == 1:
+                train_dict_2[file_label].append(arr)
+        # if is valid
         else:
-            train_dict_1[0].append(arr[train_idx])
-
-            valid_dict[0].append(arr[valid_idx])
+            valid_dict[file_label].append(arr)
 
     train_dict_1 = {key: np.concatenate(value) for key, value in train_dict_1.items()}
-    train_dict_2 = {i: np.concatenate(train_dict_2[key]) for i, key in enumerate(sorted(train_dict_2))}
+    train_dict_2 = {i: np.concatenate(val) for i, val in enumerate(train_dict_2.values())}
     valid_dict = {key: np.concatenate(value) for key, value in valid_dict.items()}
 
     return [train_dict_1, train_dict_2], valid_dict
@@ -66,15 +73,20 @@ if __name__ == '__main__':
     import argparse
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('--device', '-de', default='cpu')
-    parser.add_argument('--name', '-n', help='name of the experiment to create a folder to save weights')
+    parser.add_argument('--device', '-d', type=str, required=True)
+    parser.add_argument('--name', '-n', default='exp4',
+                        help='name of the experiment to create a folder to save weights')
+    parser.add_argument('--data-folder', '-data', default='/home/ducanh/projects/npy_data_seq/',
+                        help='path to data folder')
+    parser.add_argument('--output-folder', '-o', default='./draft',
+                        help='path to save training logs and model weights')
     args = parser.parse_args()
 
     # create data loaders
-    train_dicts, valid_dict = load_data('../../npy_data')
+    train_dicts, valid_dict = load_data(args.data_folder)
 
     augmenter = ComposeAugmenters([
-        Rotate(p=0.5, angle_range=180, angle_y_range=180, angle_z_range=180),
+        Rotate(p=0.5, angle_range=180),
         TimeWarp(p=0.5, sigma=0.2, knot_range=4)
     ])
 
@@ -87,20 +99,20 @@ if __name__ == '__main__':
     backbone = TCN(
         input_shape=(200, 3),
         how_flatten='spatial attention gap',
-        n_tcn_channels=(64,) * 7,
-        tcn_drop_rate=0.5,
-        use_spatial_dropout=False,
+        n_tcn_channels=(64,) * 5 + (128,) * 2,
+        tcn_drop_rate=0.8,
+        use_spatial_dropout=True,
         conv_norm='batch',
         attention_conv_norm='batch'
     )
     classifier = MultiFCClassifiers(
-        n_features=64,
+        n_features=128,
         n_classes=[train_set.num_classes for train_set in train_sets]
     )
-    model = CompleteModel(backbone=backbone, classifier=classifier)
+    model = CompleteModel(backbone=backbone, classifier=classifier, dropout=0.5)
 
     # create folder to save result
-    save_folder = f'./draft/{args.name}'
+    save_folder = f'{args.output_folder}/{args.name}'
     last_run = [int(exp_no.split(os.sep)[-1].split('_')[-1]) for exp_no in glob(f'{save_folder}/run_*')]
     last_run = max(last_run) + 1 if len(last_run) > 0 else 0
     save_folder = f'{save_folder}/run_{last_run}'
@@ -108,7 +120,7 @@ if __name__ == '__main__':
     # create training config
     loss_fn = nn.CrossEntropyLoss()
     optimizer = torch.optim.SGD(model.parameters(), lr=1e-3, momentum=0.9)
-    num_epochs = 5
+    num_epochs = 40
 
     flow = TrainFlow(
         model=model, loss_fn=loss_fn, optimizer=optimizer,